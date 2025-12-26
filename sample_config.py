e1c22efa7a4fec77d17d51c6f5fe8eec38cf9328
navaidkhan07860-uk <navaidkhan07860@gmail.com>
2025-12-26 17:53:29 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8521377883:AAGr4u3Shz_krhyD-YuKRrtQ0QO-hXyou_g")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", "34371650"))
+    API_HASH = os.environ.get("API_HASH", "d11bfcd1cbbb9c53acb992f1713496b")
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # log channel
